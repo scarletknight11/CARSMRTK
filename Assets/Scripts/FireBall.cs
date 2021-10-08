cd9fd54779126b66a085fb8b80f810dae1c8c079
Sanjit Singh <ss3448@scarletmail.rutgers.edu>
2021-10-08 03:55:31 -0400

    programmed weapons and added fire particle effects. some of the weapons features include fireballs, firebolt and fire explosions

---
+++ Assets/Scripts/FireBall.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DigitalRuby.PyroParticles
+{
+    public class FireBall : MonoBehaviour
+    {
+        public GameObject[] Prefabs;
+
+        private GameObject currentPrefabObject;
+        private FireBaseScript currentPrefabScript;
+        private int currentPrefabIndex;
+
+        private void UpdateEffect()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                StartCurrent();
+            }
+        }
+
+        private void BeginEffect()
+        {
+            Vector3 pos;
+            float yRot = transform.rotation.eulerAngles.y;
+            Vector3 forwardY = Quaternion.Euler(0.0f, yRot, 0.0f) * Vector3.forward;
+            Vector3 forward = transform.forward;
+            Vector3 right = transform.right;
+            Vector3 up = transform.up;
+            Quaternion rotation = Quaternion.identity;
+            currentPrefabObject = GameObject.Instantiate(Prefabs[currentPrefabIndex]);
+            currentPrefabScript = currentPrefabObject.GetComponent<FireConstantBaseScript>();
+
+            if (currentPrefabScript == null)
+            {
+                // temporary effect, like a fireball
+                currentPrefabScript = currentPrefabObject.GetComponent<FireBaseScript>();
+                if (currentPrefabScript.IsProjectile)
+                {
+                    // set the start point near the player
+                    rotation = transform.rotation;
+                    pos = transform.position + forward + right + up;
+                }
+                else
+                {
+                    // set the start point in front of the player a ways
+                    pos = transform.position + (forwardY * 10.0f);
+                }
+            }
+            else
+            {
+                // set the start point in front of the player a ways, rotated the same way as the player
+                pos = transform.position + (forwardY * 5.0f);
+                rotation = transform.rotation;
+                pos.y = 0.0f;
+            }
+
+            FireProjectileScript projectileScript = currentPrefabObject.GetComponentInChildren<FireProjectileScript>();
+            if (projectileScript != null)
+            {
+                // make sure we don't collide with other fire layers
+                projectileScript.ProjectileCollisionLayers &= (~UnityEngine.LayerMask.NameToLayer("FireLayer"));
+            }
+
+            currentPrefabObject.transform.position = pos;
+            currentPrefabObject.transform.rotation = rotation;
+        }
+
+        public void StartCurrent()
+        {
+            StopCurrent();
+            BeginEffect();
+        }
+
+        private void StopCurrent()
+        {
+            // if we are running a constant effect like wall of fire, stop it now
+            if (currentPrefabScript != null && currentPrefabScript.Duration > 10000)
+            {
+                currentPrefabScript.Stop();
+            }
+            currentPrefabObject = null;
+            currentPrefabScript = null;
+        }
+
+    }
+}
